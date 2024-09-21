61c8a7323cc41b4494ba0a19981e31cac3cf9377
Kavish Ambani <kavishkkambani@gmail.com>
2024-09-21 12:46:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <> 
+      <div className="text-3xl text-[#fff] bg-[#1c1d1f] flex flex-col justify-center items-center min-h-screen">
+        <h1>Hi, Kavish here</h1>  
+      </div>
+    </>
+  )
+}
+
+export default App
